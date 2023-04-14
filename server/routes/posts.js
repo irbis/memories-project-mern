b7192c6d32ad91e0b1cd616361bd4dce0615ddde
Alexsey Nadtochey <a.nadtochey@gmail.com>
2023-04-14 19:00:33 +0300

    First versions of Get and Post handlers of messages were created.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,9 +1,10 @@
 import express from "express";
 
+import { getPosts, createPost } from "../controllers/posts.js";
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('THIS WORKS!')
-});
+router.get('/', (req, res) => getPosts(req, res));
+router.post('/', (req, res) => createPost(req, res));
 
 export default router;
