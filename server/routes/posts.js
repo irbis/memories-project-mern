8a36a1bcb53bd8ba4d83e4ec92b3a0dc40112496
Alexsey Nadtochey <a.nadtochey@gmail.com>
2023-04-28 15:34:31 +0300

    Ability to read and update posts from a database was added.

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,10 +1,11 @@
 import express from "express";
 
-import { getPosts, createPost } from "../controllers/posts.js";
+import { getPosts, createPost, updatePost } from "../controllers/posts.js";
 
 const router = express.Router();
 
 router.get('/', (req, res) => getPosts(req, res));
 router.post('/', (req, res) => createPost(req, res));
+router.patch('/:id', updatePost);
 
 export default router;
